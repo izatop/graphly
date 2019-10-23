b91964fa9eccca6b25678e580b1340eb1865fcc7
Artur Bier <izatop@gmail.com>
2019-10-23 12:17:28 +0300

    fix: graphly-cli compose --watch mode added

--- packages/cli/src/Command/ComposeCommand.ts
+++ packages/cli/src/Command/ComposeCommand.ts
@@ -1,8 +1,14 @@
 import {Argument, Command, CommandDefinition, FlagOption, Option} from "@sirian/console";
+import {debounce} from "@sirian/decorators";
+import watch from "node-watch";
 import * as path from "path";
 import {Composer} from "../Composer";
 
 export class ComposeCommand extends Command {
+    protected get verbose(): boolean {
+        return this.getOption("verbose");
+    }
+
     public static configure(def: CommandDefinition) {
         def
             .addArguments([new Argument({name: "file", multiple: true, required: true})])
@@ -11,18 +17,43 @@ export class ComposeCommand extends Command {
                 new FlagOption({name: "watch", shortcut: "w"}),
             ])
             .setDescription("GraphQL Server");
+
+        process.on("SIGINT", () => process.exit());
     }
 
     public async execute() {
-        const files = this.getArgument("file");
-        const verbose = this.getOption("verbose");
+        const files: string[] = this.getArgument("file");
+        const watchFlag: boolean = this.getOption("watch");
 
+        this.compose(files);
+        if (watchFlag) {
+            const watchOptions = {recursive: true, filter: /^.+\.(ts|js)$/};
+            const watchDirectory = files.map((file) => path.dirname(file));
+            // tslint:disable-next-line:no-console
+            this.log("Schema watching:", watchDirectory);
+            watch(watchDirectory, watchOptions, (e, file) => {
+                // tslint:disable-next-line:no-console
+                this.log("Schema", e, file);
+                this.compose(files);
+            });
+        }
+    }
+
+    public log(...msg: any) {
+        if (this.verbose) {
+            // tslint:disable-next-line:no-console
+            console.log(...msg);
+        }
+    }
+
+    @debounce(1000)
+    protected compose(files: string[]) {
         for (const schemaPath of files) {
             const name = path.basename(schemaPath);
             const composer = new Composer({
                 name,
                 schemaPath,
-                verbose,
+                verbose: this.verbose,
             });
 
             composer.save();
