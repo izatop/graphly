4772b7ede1be651ce6a93d69fe5376dd9f279ac7
abir <izatop@gmail.com>
2021-01-22 19:01:54 +0300

    fix: subscriptions

--- packages/type/src/Type/SubscriptionType.ts
+++ packages/type/src/Type/SubscriptionType.ts
@@ -1,6 +1,6 @@
 import {OutputType, ResolverArgs} from "../Interface";
 
-export type Subscription<T> = AsyncIterator<T>;
+export type Subscription<T> = AsyncIterable<T>;
 export type SubscribeFunction<T> = (...args: ResolverArgs[]) => Subscription<T>;
 export type SubscribeFunctionAsync<T> = (...args: ResolverArgs[]) => Promise<Subscription<T>>;
 
